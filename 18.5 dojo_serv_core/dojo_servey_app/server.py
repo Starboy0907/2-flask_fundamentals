214b26723270dfd323c6bbd0a5c1f233b018c8a4
Starboy0907 <95019285+Starboy0907@users.noreply.github.com>
2022-03-06 20:58:16 -0800

    Dojo Survey Flask Assignment

--- 18.5 dojo_serv_core/dojo_servey_app/server.py
+++ 18.5 dojo_serv_core/dojo_servey_app/server.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
-import random
+from flask import Flask, render_template, request,redirect,session
 app = Flask(__name__)
-app.secret_key = 'frankadank'
+app.secret_key = "cash"
 
 @app.route('/')
 def index():
@@ -9,23 +8,16 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    print('GOT POST INFO')
+    print("GOT POST INFO")
     print(request.form)
     session['name'] = request.form['name']
     session['location'] = request.form['location']
-    session['language'] = request.form['language']
-    session['optradio'] = request.form['optradio']
-    session['text'] = request.form['text']
-    session['remember'] = request.form['remember']
+    session['favorite_lang'] = request.form['favorite_lang']
     return redirect('/results')
 
 @app.route('/results')
 def results():
     return render_template('results.html')
 
-
-
-
-
-if __name__=="__main__":
+if __name__ =="__main__":
     app.run(debug=True)
